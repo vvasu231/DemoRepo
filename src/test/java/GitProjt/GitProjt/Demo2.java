308bcd47e0015c96ca7cc1c5142efee43c60be93
vvasu231 <venkatavasu231@gmail.com>
2022-03-06 15:44:58 +0530

    I am modifies the code for future perpouse

---
+++ src/test/java/GitProjt/GitProjt/Demo2.java
@@ -0,0 +1,20 @@
+package GitProjt.GitProjt;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+	@Test
+	public void hello() {
+		System.out.println("@Test: hello");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
